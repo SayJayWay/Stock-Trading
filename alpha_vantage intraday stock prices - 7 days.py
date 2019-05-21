b4c4523cba6ddd6996cf04dce8d755dc5d17ef06
SayJayWay <32993491+SayJayWay@users.noreply.github.com>
2019-05-20 20:43:53 -0400

    Update alpha_vantage intraday stock prices - 7 days.py

--- alpha_vantage intraday stock prices - 7 days.py
+++ alpha_vantage intraday stock prices - 7 days.py
@@ -1,7 +1,7 @@
 from alpha_vantage.timeseries import TimeSeries
 import matplotlib.pyplot as plt
-ts = TimeSeries(key='WFJ5OP8E2OF56H55',output_format='pandas')
-data, meta_data = ts.get_intraday(symbol='AMD',interval='1min', outputsize='full')
+ts = TimeSeries(key='',output_format='pandas') # insert key found from AlphaVantage site
+data, meta_data = ts.get_intraday(symbol='AMD',interval='1min', outputsize='full') 
 #print(data)
 data.to_csv('AMDalpha_vantage.csv')
 #data['4. close'].plot()
