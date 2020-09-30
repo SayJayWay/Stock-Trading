1c45f70d53802e7a574b77649a3387d8a1795067
SayJayWay <32993491+SayJayWay@users.noreply.github.com>
2020-09-29 22:47:45 -0400

    update

--- IB_Backtest/algo_analysis.py
+++ IB_Backtest/algo_analysis.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from pylab import plt
 import scipy.stats as scs
+#
 
 class Algo_Analysis:
     '''Algo_Analysis is a class created to perform various statistical analyses:
