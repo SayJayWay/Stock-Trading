a67b40f183833fe2e96082f52d8d991411cbb3d9
SayJayWay <32993491+SayJayWay@users.noreply.github.com>
2020-11-22 13:20:56 -0500

    Comment changes

--- IB_Backtest/trade_functions.py
+++ IB_Backtest/trade_functions.py
@@ -8,36 +8,40 @@ nest_asyncio.apply()
 
 from ib_insync import *
 
-def make_contract(symbol, sec_type, exch, prim_exch, curr):
-    Contract.m_symbol = symbol
-    Contract.m_secType = sec_type
-    Contract.m_exchange = exch
-    Contract.m_primaryExchange = prim_exch
-    Contract.m_currency = curr
-    return Contract
+#%% This is if using ib-native
+# def make_contract(symbol, sec_type, exch, prim_exch, curr):
+#     Contract.m_symbol = symbol
+#     Contract.m_secType = sec_type
+#     Contract.m_exchange = exch
+#     Contract.m_primaryExchange = prim_exch
+#     Contract.m_currency = curr
+#     return Contract
 
-def make_order(action, quantity, price = None):
-    if price is not None:
-        order = Order()
-        order.m_orderType = 'LMT'
-        order.m_totalQuantity = quantity
-        order.m_action = action # Buy or Sell
-        order.m_lmtPrice = price
-    else:
-        order = Order()
-        order.m_orderType = 'MKT'
-        order.m_totalQuantity = quantity
-        order.m_action = action # Buy or Sell
-        
-    return order
+# def make_order(action, quantity, price = None):
+#     if price is not None:
+#         order = Order()
+#         order.m_orderType = 'LMT'
+#         order.m_totalQuantity = quantity
+#         order.m_action = action # Buy or Sell
+#         order.m_lmtPrice = price
+#     else:
+#         order = Order()
+#         order.m_orderType = 'MKT'
+#         order.m_totalQuantity = quantity
+#         order.m_action = action # Buy or Sell    
+#     return order
 
+#%% 
 def main():
     ib = IB()
     ib.connect('127.0.0.1', port = 7497, clientId = 556)
     print('CONNECTED')
     
+    # ib-native method
     # cont = make_contract('TSLA', 'STK', 'SMART', 'SMART', 'USD')
     # offer = make_order('BUY', 1, 100)
+    
+    # ib_insync method  
     contract = Stock('AAPL', 'SMART', 'USD')
     ib.qualifyContracts(contract)
     order = MarketOrder('SELL', 1) 
