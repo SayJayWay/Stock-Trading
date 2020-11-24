dc0c7402e332b9172b179b30f1139a87952fcfad
SayJayWay <32993491+SayJayWay@users.noreply.github.com>
2020-11-23 22:46:01 -0500

    Update tf_MACD_OHLC_analysis.py
    Fleshed out MACD_OHLC_analysis.py
    Accuracy stuck at 0.5 for some reason...

--- IB_Backtest/tf_MACD_OHLC_analysis.py
+++ IB_Backtest/tf_MACD_OHLC_analysis.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 df = pd.read_csv('../stock_dfs/AAPL.csv')
 
@@ -17,14 +18,22 @@ for lag in range(1,4):
     
 df['Adj_Close_lead_5'] = df['Adj Close'].shift(-5)
 
-df['sign_lead_5'] = np.where(df['Adj_Close_lead_5'] > df['Adj Close'], 1, -1)
+df['Output_1'] = np.where(df['Adj_Close_lead_5'] > df['Adj Close'], 0.99, 0.01)
+df['Output_2'] = np.where(df['Adj_Close_lead_5'] <= df['Adj Close'], 0.99, 0.01)
 
 df.dropna(inplace = True)
+df.reset_index(inplace = True)
 
+inputs = df[['MACD_lag_1', 'MACD_lag_2', 'MACD_lag_3',
+             'Open_lag_1', 'Open_lag_2', 'Open_lag_3', 'High_lag_1',
+             'High_lag_2', 'High_lag_3', 'Low_lag_1', 'Low_lag_2', 'Low_lag_3',
+             'Adj_Close_lag_1', 'Adj_Close_lag_2', 'Adj_Close_lag_3']]
 
-learning_rate = 0.0001
-batch_size = 100
-update_tep = 10
+outputs = df[['Output_1', 'Output_2']]
+
+learning_rate = 0.00001
+batch_size = 2
+update_step = 7
 
 input_nodes = 15
 layer_1_nodes = 15
@@ -37,7 +46,6 @@ target_output = tf.placeholder(tf.float32, [None, output_nodes])
 
 layer_1 = tf.Variable(tf.random_normal([input_nodes, layer_1_nodes]))
 layer_1_bias = tf.Variable(tf.random_normal([layer_1_nodes]))
-
 layer_2 = tf.Variable(tf.random_normal([layer_1_nodes, layer_2_nodes]))
 layer_2_bias = tf.Variable(tf.random_normal([layer_2_nodes]))
 
@@ -54,18 +62,37 @@ l3_output = tf.nn.relu(tf.matmul(l2_output, layer_3) + layer_3_bias)
 ntwk_output_1 = tf.matmul(l3_output, out_layer) + out_layer_bias
 ntwk_output_2 = tf.nn.softmax(ntwk_output_1)
 
-cf = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=ntwk_output_1, labels = target_output))
+cf = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits=ntwk_output_1, labels = target_output))
 
 ts = tf.train.GradientDescentOptimizer(learning_rate).minimize(cf)
 
-cp = tf.equal(tf.argmax(ntwk_output_2, 1), tf.argmax(df['sign_lead_5']))
+cp = tf.equal(tf.argmax(ntwk_output_2, 1), tf.argmax(outputs))
 
 acc = tf.reduce_mean(tf.cast(cp, tf.float32))
 
+def next_batch(num, data, labels):
+    '''
+    Return a total of 'num' random samples and labels. 
+    '''
+    idx = np.arange(0 , len(data))
+    np.random.shuffle(idx)
+    idx = idx[:num]
+    # print(idx)
+    data_shuffle = [data.iloc[i] for i in idx]
+    labels_shuffle = [labels.iloc[i] for i in idx]
+
+    return np.asarray(data_shuffle), np.asarray(labels_shuffle)
+
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
-    num_epochs = 10
+    num_epochs = 1
     for epoch in range(num_epochs):
         total_cost = 0
-        for _ in range(len(df['Adj Close']) / batch_size):
-            batch_x, batch_y = 
+        for _ in range(int(len(df['Adj Close']) / batch_size)):
+            batch_x, batch_y = next_batch(batch_size, inputs, outputs)
+            t, c = sess.run([ts, cf], feed_dict = {network_input: batch_x, target_output: batch_y})
+            total_cost += c
+            print('Epoch', epoch, 'completed out of', num_epochs, 'loss:', total_cost)
+        
+        test_x, test_y = next_batch(batch_size, inputs, outputs)
+        print('Accuracy:', acc.eval({network_input:test_x, target_output: test_y}))
